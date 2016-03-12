6f1bba53ad766d3aa2ac0b035f555ca45954b98e
Justin Cook <jhcook@gmail.com>
2016-03-12 13:19:40 +0000

    Error on rm bombs script, so || /bin/true

--- centos7/scripts/vagrant.sh
+++ centos7/scripts/vagrant.sh
@@ -4,4 +4,4 @@ mkdir -pm 700 /home/vagrant/.ssh
 curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
-rm /etc/udev/rules.d/70-persistent-net.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules || /bin/true
