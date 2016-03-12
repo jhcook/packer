6038d5b8b3023d1632d8ed4326e18c267c104e1e
Justin Cook <jhcook@gmail.com>
2016-03-12 12:06:31 +0000

    Update CentOS6 build to use local mirror then revert on pack

--- centos6/scripts/cleanup.sh
+++ centos6/scripts/cleanup.sh
@@ -1,4 +1,12 @@
+# Change to default CentOS6 repo config
+/usr/bin/rm -f /etc/yum.repos.d/CentOS-Base.repo.repo
+/usr/bin/mv /etc/yum.repos.d/CentOS-Base.repo.orig \
+  /etc/yum.repos.d/CentOS-Base.repo
+
+# Remove VBoxAdditions iso and Ruby gems
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
+
+# Clean yum repo and reset history
 yum history new
 yum clean all
