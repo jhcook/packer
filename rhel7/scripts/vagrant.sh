b9b301dea501c341a012ec4bcc55092f5a26ec41
Justin Cook <jhcook@gmail.com>
2016-09-01 13:24:04 +0100

    Set UseDNS to no in sshd_config

--- rhel7/scripts/vagrant.sh
+++ rhel7/scripts/vagrant.sh
@@ -4,4 +4,5 @@ mkdir -pm 700 /home/vagrant/.ssh
 curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+sed -ie 's/^#*UseDNS\ [a-zA-Z]\+/UseDNS no/' /etc/ssh/sshd_config
 rm -f /etc/udev/rules.d/70-persistent-net.rules || /bin/true
