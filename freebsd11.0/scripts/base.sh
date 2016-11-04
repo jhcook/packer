a547102f2b3118e65cbc2b63c65987397346d5e8
Justin Cook <jhcook@gmail.com>
2016-11-04 12:13:04 +0000

    remove update ports from base and add swapfile on freebsd11.0

--- freebsd11.0/scripts/base.sh
+++ freebsd11.0/scripts/base.sh
@@ -2,15 +2,10 @@
 echo -e 'hostname="freebsd11-vbox"' >> /etc/rc.conf
 
 # Create a 512 MiB swap file
-#/bin/dd if=/dev/zero of=/swapfile bs=4096 count=131072
-#/bin/chmod 600 /swapfile
-#echo -e 'swapfile="/swapfile"' >> /etc/rc.conf
-#/sbin/mdconfig -a -t vnode -f /swapfile -u 0 && /sbin/swapon /dev/md0
-
-# Update ports
-mkdir -p /usr/ports
-cd /usr/ports
-/usr/sbin/portsnap --interactive fetch extract
+/bin/dd if=/dev/zero of=/swapfile bs=4096 count=131072
+/bin/chmod 600 /swapfile
+echo -e 'swapfile="/swapfile"' >> /etc/rc.conf
+/sbin/mdconfig -a -t vnode -f /swapfile -u 0 && /sbin/swapon /dev/md0
 
 # Install and update sudo
 #cd /usr/ports/security/sudo
