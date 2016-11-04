0befe5a8e740e2977303bd6556bea70772af696f
Justin Cook <jhcook@gmail.com>
2016-11-04 14:08:01 +0000

    update all freebsd base swapfiles

--- freebsd10.3/scripts/base.sh
+++ freebsd10.3/scripts/base.sh
@@ -2,10 +2,9 @@
 echo -e 'hostname="freebsd10-vbox"' >> /etc/rc.conf
 
 # Create a 512 MiB swap file
-/bin/dd if=/dev/zero of=/swapfile bs=4096 count=131072
-/bin/chmod 600 /swapfile
-echo -e 'swapfile="/swapfile"' >> /etc/rc.conf
-/sbin/mdconfig -a -t vnode -f /swapfile -u 0 && /sbin/swapon /dev/md0
+/bin/dd if=/dev/zero of=/usr/swap0 bs=1m count=512
+/bin/chmod 600 /usr/swap0
+echo "md99    none    swap    sw,file=/usr/swap0,late    0    0" >> /etc/fstab
 
 # Install and update sudo
 #cd /usr/ports/security/sudo
