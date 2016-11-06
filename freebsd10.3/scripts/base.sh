955a04ce7221b6c1e1048ecf3adba699e34d33ed
Justin Cook <jhcook@gmail.com>
2016-11-06 18:50:24 +0000

    remove the swapfile addition since it has a swap partition

--- freebsd10.3/scripts/base.sh
+++ freebsd10.3/scripts/base.sh
@@ -2,9 +2,9 @@
 echo -e 'hostname="freebsd10-vbox"' >> /etc/rc.conf
 
 # Create a 512 MiB swap file
-/bin/dd if=/dev/zero of=/usr/swap0 bs=1m count=512
-/bin/chmod 600 /usr/swap0
-echo "md99    none    swap    sw,file=/usr/swap0,late    0    0" >> /etc/fstab
+#/bin/dd if=/dev/zero of=/usr/swap0 bs=1m count=512
+#/bin/chmod 600 /usr/swap0
+#echo "md99    none    swap    sw,file=/usr/swap0,late    0    0" >> /etc/fstab
 
 # Install and update sudo
 #cd /usr/ports/security/sudo
